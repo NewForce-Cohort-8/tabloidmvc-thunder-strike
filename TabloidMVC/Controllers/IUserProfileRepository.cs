76e6efce8dcaa8d3a89c00c8373f22b2e2bb7c48
JuanAlcalde <jjalcalde001@icloud.com>
2024-01-23 02:47:48 -0500

    completed user profile list

---
+++ TabloidMVC/Controllers/IUserProfileRepository.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
+using System.Security.Claims;
+using TabloidMVC.Models.ViewModels;
+using TabloidMVC.Repositories;
+using TabloidMVC.Models;
+namespace TabloidMVC.Controllers
+{
+
+    public class UserProfileController : Controller
+    {
+
+        private readonly IUserProfileRepository _userProfileRepo;
+
+
+        public UserProfileController(IUserProfileRepository userProfileRepository)
+        {
+            _userProfileRepo = userProfileRepository;
+        }
+        // GET: UserProfileController
+
+        public ActionResult Index()
+        {
+            List<UserProfile> userProfile = _userProfileRepo.GetAllUserProfiles();
+
+            return View(userProfile);
+        }
+
+        // GET: UserProfileController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserProfileController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserProfileController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserProfileController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserProfileController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- TabloidMVC/Repositories/UserProfileRepository.cs
+++ TabloidMVC/Repositories/UserProfileRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using TabloidMVC.Models;
 using TabloidMVC.Utils;
 
@@ -7,6 +8,54 @@ namespace TabloidMVC.Repositories
     public class UserProfileRepository : BaseRepository, IUserProfileRepository
     {
         public UserProfileRepository(IConfiguration config) : base(config) { }
+        public List<UserProfile> GetAllUserProfiles()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                SELECT u.Id, u.FirstName, u.LastName, u.DisplayName, u.Email,
+                       u.CreateDateTime, u.ImageLocation, u.UserTypeId,
+                       ut.[Name] AS UserTypeName
+                FROM UserProfile u
+                LEFT JOIN UserType ut ON u.UserTypeId = ut.Id
+                ORDER BY u.DisplayName ASC";
+                    var reader = cmd.ExecuteReader();
+
+                    var UserProfiles = new List<UserProfile>();
+
+                    while (reader.Read())
+                    {
+                        UserProfiles.Add(NewUserProfileFromReader(reader));
+                    }
+
+                    reader.Close();
+
+                    return UserProfiles;
+                }
+            }
+        }
+        private UserProfile NewUserProfileFromReader(SqlDataReader reader)
+        {
+            return new UserProfile()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                DisplayName = reader.GetString(reader.GetOrdinal("DisplayName")),
+                Email = reader.GetString(reader.GetOrdinal("Email")),
+                CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                ImageLocation = DbUtils.GetNullableString(reader, "ImageLocation"),
+                UserTypeId = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                UserType = new UserType()
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                    Name = reader.GetString(reader.GetOrdinal("UserTypeName"))
+                },
+            };
+        }
 
         public UserProfile GetByEmail(string email)
         {
